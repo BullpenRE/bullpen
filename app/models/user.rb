33e2e0d287119484940e5b6c8c75b94002098f68
“SBorozenets” <s.borozenet@gmail.com>
2020-09-08 21:29:15 +0300

    after review

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,3 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :confirmable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :confirmable, :trackable
 end
