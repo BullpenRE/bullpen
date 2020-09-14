a1511548148ba659901a979e35d4ed1b38c7c4f9
Roger Graves <rogergraves@gmail.com>
2020-09-14 12:05:07 -0700

    Suppress generations for rspec

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,17 @@ module Bullpen
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    # Suppress automatic generation of certain types of specs
+    # Per https://www.codewithjason.com/get-rspec-skip-view-specs-generate-scaffolds/
+    config.generators do |generate|
+      generate.test_framework :rspec,
+                              fixtures: false,
+                              view_specs: false,
+                              helper_specs: false,
+                              routing_specs: false,
+                              request_specs: false,
+                              controller_specs: false
+    end
   end
 end
