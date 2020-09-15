fd52c1ad08868e94b6255c6d8745923277250d3e
Roger Graves <rogergraves@gmail.com>
2020-09-15 12:59:10 -0700

    Disqualify documenation requirement from CodeClimate and address known issues

--- app/controllers/join_controller.rb
+++ app/controllers/join_controller.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 class JoinController < ApplicationController
   def index; end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,4 @@
+# frozen_string_literal: true
+# In general using helpers too much goes to a bad place. Be careful...
 module ApplicationHelper
 end
