8171a01ad086cb231c47f6d7b4c9304da2c5ba74
“SBorozenets” <s.borozenet@gmail.com>
2020-09-16 08:33:03 +0300

    BP-16 Create page after reset password

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   get '/join', to: 'join#index'
-  devise_for :users
+  devise_for :users, controllers: { passwords: 'passwords' }
+  devise_scope :user do
+    get '/password_reset', to: 'passwords#password_reset'
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  # mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
