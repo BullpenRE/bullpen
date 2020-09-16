7a1828b919008f5621027ef9eeb569204e3ae5ef
EC2 Default User <ec2-user@ip-172-31-38-210.us-east-2.compute.internal>
2020-09-16 16:35:34 +0000

    changes after review

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
 class PasswordsController < Devise::PasswordsController
   protected
+  
   def after_sending_reset_password_instructions_path_for(resource_name)
     password_reset_path(resource_name)
   end
