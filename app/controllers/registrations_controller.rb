5e8b0772b0c6a645b5653bc46639192c529fb976
“SBorozenets” <s.borozenet@gmail.com>
2020-09-21 18:41:40 +0300

    try to fix bug

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -18,6 +18,10 @@ class RegistrationsController < Devise::RegistrationsController
 
   protected
 
+  def after_sign_in_path_for(resource)
+    registration_steps_path(current_user)
+  end
+
   def after_sign_up_path_for(resource)
     registration_steps_path
   end
