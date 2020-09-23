769706dd47def3ebf7c55b91ad54be8d967f6f94
“SBorozenets” <s.borozenet@gmail.com>
2020-09-23 22:54:59 +0300

    BP-29 Prevent signing up with an email that already exists

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
 
   resources :registration_steps
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  post '/check_users_email', to: 'check_email#check_users_email'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 
   root 'join#index'
