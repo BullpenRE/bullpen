dc64f2a296e783b0d44d7c49f2acfea925d4f9d6
Roger Graves <rogergraves@gmail.com>
2020-10-02 13:49:46 -0700

    Follow CodeClimate suggestions

--- app/models/asset_class.rb
+++ app/models/asset_class.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AssetClass < ApplicationRecord
   validates :description, presence: true
 end
--- app/models/freelancer_asset_class.rb
+++ app/models/freelancer_asset_class.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class FreelancerAssetClass < ApplicationRecord
   validates :freelancers_id, uniqueness: { scope: :asset_classes_id }
 end
