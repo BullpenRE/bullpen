853a2bd8b718fdff7237fc2834b685aa8b69f281
GalinaKr <galinakr@github.com>
2020-10-05 12:02:21 +0300

    fix codeclimate

--- app/controllers/employer_registartion_controller.rb
+++ app/controllers/employer_registartion_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
-class EmployerRegistrationsController < RegistrationStepsController
-  
+class EmployerRegistrationsController < RegistrationsController
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :phone_number, :is_employer)
   end
