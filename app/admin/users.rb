83cd5ba727f6141b35150e598e38a554d058464e
Roger Graves <rogergraves@gmail.com>
2020-10-06 16:34:38 -0700

    Add support for AssetClass and RealEstateSkill

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,5 +1,4 @@
 ActiveAdmin.register User do
-
   permit_params :email, :first_name, :last_name, :confirmed_at
 
   index do
@@ -49,19 +48,4 @@ ActiveAdmin.register User do
     end
 
   end
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :sign_in_count, :current_sign_in_at, :last_sign_in_at, :current_sign_in_ip, :last_sign_in_ip, :confirmation_token, :confirmed_at, :confirmation_sent_at, :unconfirmed_email, :first_name, :last_name
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :sign_in_count, :current_sign_in_at, :last_sign_in_at, :current_sign_in_ip, :last_sign_in_ip, :confirmation_token, :confirmed_at, :confirmation_sent_at, :unconfirmed_email, :first_name, :last_name]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
 end
