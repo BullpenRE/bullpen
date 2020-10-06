b3358814d791d83af672f69971269a841bb5ae81
Roger Graves <rogergraves@gmail.com>
2020-10-06 16:24:19 -0700

    Add ActiveAdmin support for User

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,5 +1,6 @@
 ActiveAdmin.register User do
 
+  permit_params :email, :first_name, :last_name, :confirmed_at
 
   index do
     column :email
@@ -8,11 +9,46 @@ ActiveAdmin.register User do
     column :created_at
     column :last_sign_in_at
     column :confirmed_at
+
+    actions defaults: true
   end
 
   filter :email
   filter :confirmed, as: :boolean
 
+  form do |f|
+    f.inputs 'User Info' do
+      f.input :email
+      f.input :password, label: 'Password (if unchanged, leave blank)'
+      f.input :first_name
+      f.input :last_name
+      f.input :confirmed_at, label: 'Confirmed (UTC)'
+      f.actions
+    end
+  end
+
+  controller do
+    def update
+      Rails.logger.info "!!!!!!! running update now !!!!!!!!"
+      user = User.find(params[:id])
+      error_message = nil
+      params[:user].delete('password') if params[:user][:password].blank?
+
+      begin
+        user.update!(params.permit![:user])
+      rescue StandardError => e
+        error_message = e.message
+      end
+
+      redirect_to admin_user_path(user), notice: ('Successfully updated!' unless error_message), alert: error_message
+    end
+
+    def create
+      @user = User.new(params.permit![:user])
+      super
+    end
+
+  end
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
