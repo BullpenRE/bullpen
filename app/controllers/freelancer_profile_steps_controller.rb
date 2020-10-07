8028f1e0856fcbf2c06df3c36f273e16624e58e5
“SBorozenets” <s.borozenet@gmail.com>
2020-10-07 19:26:53 +0300

    BP-43 Create Freelancers Professional History view

--- app/controllers/freelancer_profile_steps_controller.rb
+++ app/controllers/freelancer_profile_steps_controller.rb
@@ -2,7 +2,7 @@
 
 class FreelancerProfileStepsController < ApplicationController
   include Wicked::Wizard
-  steps :skills_page, :other_step
+  steps :skills_page, :professional_history
 
   def show
     @user = current_user
@@ -16,8 +16,13 @@ class FreelancerProfileStepsController < ApplicationController
     @freelancer_profile = @user.freelancer_profile
 
     skills_page_save if wizard_value(step) == :skills_page
+    professional_history_save if wizard_value(step) == :professional_history
 
     render_wizard @user
+  end
+
+  def professional_history_save
+    @freelancer_profile.update_attributes(history_params)
   end
 
   def skills_page_save
@@ -29,5 +34,10 @@ class FreelancerProfileStepsController < ApplicationController
     params[:freelancer_profile][:freelancer_asset_classes]&.reject(&:blank?)&.each do |asset|
       FreelancerAssetClass.create(freelancer_profile_id: @freelancer_profile.id, asset_class_id: asset)
     end
+  end
+
+  def history_params
+    params.require(:freelancer_profile)
+      .permit(:professional_title, :professional_years_experience, :professional_summary)
   end
 end
