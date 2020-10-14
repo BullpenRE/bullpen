9bf786bf0abfa93ec9282d5e63ccda0427064c9c
GalinaKr <galinakr@github.com>
2020-10-14 07:35:03 +0300

    Create Employers ‘How many people work in your company?’ view

--- app/controllers/employer_profile_steps_controller.rb
+++ app/controllers/employer_profile_steps_controller.rb
@@ -3,7 +3,9 @@
 class EmployerProfileStepsController < ApplicationController
   include Wicked::Wizard
 
-  steps :about_company
+  AVAILABLE_EMPLOYEE_COUNT = %w['1-10', '11-50', '51-100', '101+']
+
+  steps :about_company, :employee_count
 
   def show
     @user = current_user
@@ -20,12 +22,17 @@ class EmployerProfileStepsController < ApplicationController
     @employer_profile = @user.employer_profile
 
     about_company_save if wizard_value(step) == :about_company
+    employee_count_save if wizard_value(step) == :employee_count
 
     render_wizard @user
   end
 
   def about_company_save
     @employer_profile.update_attributes(company_params)
+  end
+
+  def employee_count_save
+
   end
 
   def company_params
