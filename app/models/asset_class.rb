0bf3aedb81626279faf01bfb1eb7725ee69e1072
Roger Graves <rogergraves@gmail.com>
2020-10-14 18:00:02 -0700

    Improve relationships between freelancer_profile and other many to many models

--- app/models/asset_class.rb
+++ app/models/asset_class.rb
@@ -3,4 +3,5 @@
 class AssetClass < ApplicationRecord
   validates :description, presence: true, uniqueness: true
   has_many :freelancer_asset_classes, dependent: :destroy
+  has_many :freelancer_profiles, through: :freelancer_asset_classes
 end
