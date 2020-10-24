10d9a88bbdc236aced03ce8cb349da10cea7f0c8
Roger Graves <rogergraves@gmail.com>
2020-10-24 11:06:51 -0700

    Undo table drop

--- db/migrate/20201023223210_change_asset_class_to_sector.rb
+++ db/migrate/20201023223210_change_asset_class_to_sector.rb
@@ -1,6 +1,5 @@
 class ChangeAssetClassToSector < ActiveRecord::Migration[6.0]
   def change
-    drop_table :sectors if ActiveRecord::Base.connection.data_source_exists? 'sectors'
     rename_table :asset_classes, :sectors
     rename_table :freelancer_asset_classes, :freelancer_sectors
     rename_column :freelancer_sectors, :asset_class_id, :sector_id
