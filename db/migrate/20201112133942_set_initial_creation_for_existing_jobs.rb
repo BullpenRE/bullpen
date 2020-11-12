65829f967a536f067a600d7d92d0f8e2df9d7080
“SBorozenets” <s.borozenet@gmail.com>
2020-11-12 22:27:32 +0200

    changes after review

--- db/migrate/20201112133942_set_initial_creation_for_existing_jobs.rb
+++ db/migrate/20201112133942_set_initial_creation_for_existing_jobs.rb
@@ -2,8 +2,4 @@ class SetInitialCreationForExistingJobs < ActiveRecord::Migration[6.0]
   def up
     Job.update_all(initial_creation: false)
   end
-
-  def down
-    Job.update_all(initial_creation: true)
-  end
 end
