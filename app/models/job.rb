03ee87ec7d7520cb746a74607fe57bf837b68016
“SBorozenets” <s.borozenet@gmail.com>
2020-11-17 16:28:37 +0200

    change time_zone field

--- app/models/job.rb
+++ app/models/job.rb
@@ -15,4 +15,5 @@ class Job < ApplicationRecord
   enum position_length: { 'long-term': 0, 'temporary': 1 }
   enum hours_needed: { 'part-time': 0, 'on-call': 1, 'project-based': 2 }
   enum required_experience: { 'junior': 0, 'intermediate': 1, 'senior': 2 }
+  enum time_zone: { 'HST': 0, 'AKST': 1, 'PST': 2, 'MST': 3, 'CST': 4, 'EST': 5 }
 end
