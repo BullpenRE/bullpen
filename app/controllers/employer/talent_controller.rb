de3d0172822e1706c88ca2ce7d8941c05ecc8f50
Goncharenko2013 <tgoncharenko@gmail.com>
2020-11-18 21:48:53 +0200

    [#BP-123] Employer Talent view was created

--- app/controllers/employer/talent_controller.rb
+++ app/controllers/employer/talent_controller.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 class Employer::TalentController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @freelancer_profiles = FreelancerProfile.where(draft: false)
+  end
+
 end
