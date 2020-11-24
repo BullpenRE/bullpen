d3ee6333010d3cb3225eb741814d1524a39a2133
Goncharenko2013 <tgoncharenko@gmail.com>
2020-11-24 18:52:36 +0200

    BP-123 BP-125 fix in accordance with reviewer's suggestions

--- app/controllers/employer/talent_controller.rb
+++ app/controllers/employer/talent_controller.rb
@@ -4,7 +4,12 @@ class Employer::TalentController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @freelancer_profiles = FreelancerProfile.where(draft: false)
+    @freelancer_profiles = FreelancerProfile.includes(:real_estate_skills,
+                                                      :sectors,
+                                                      :softwares,
+                                                      :freelancer_certifications,
+                                                      :freelancer_profile_educations,
+                                                      :freelancer_profile_experiences).where(draft: false)
   end
 
 end
