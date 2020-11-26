0c0637816760def3eb3f7a3cbb5a1944a1f25fc1
GalinaKr <galinakr@github.com>
2020-11-26 21:06:09 +0200

    add mailer for rejected freelancer

--- app/mailers/freelancer_mailer.rb
+++ app/mailers/freelancer_mailer.rb
@@ -3,9 +3,18 @@
 class FreelancerMailer < ApplicationMailer
   default from: 'support@bullpenre.com'
 
-  def freelancer_approved_email
+  def freelancer_approved
+    set_params
+    mail(to: @user.email, subject: 'Congratulations - Welcome to Bullpen')
+  end
+
+  def freelancer_rejected
+    set_params
+    mail(to: @user.email, subject: 'Your Bullpen Application')
+  end
+
+  def set_params
     @user = params[:user]
     @url = ENV['WEBSITE_URL']
-    mail(to: @user.email, subject: 'Congratulations - Welcome to Bullpen')
   end
 end
