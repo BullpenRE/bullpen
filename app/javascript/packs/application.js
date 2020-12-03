fb13bfd500ec042a2594e0ab553f06eb1380bb34
Roger Graves <rogergraves@gmail.com>
2020-12-03 13:02:12 -0800

    Correct path of side bar library

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,7 +14,7 @@ require("bootstrap-select")
 require("select2")
 require("typeface-nunito-sans")
 require("typeface-ubuntu")
-require("@ctrlmaniac/simpler-sidebar")
+require("@ctrlmaniac/simpler-sidebar/lib")
 
 import 'src/stylesheets/application'
 const images = require.context('../images', true)
