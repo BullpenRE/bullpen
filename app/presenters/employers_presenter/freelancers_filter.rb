97bfa98ef4edb817e6b3bae5da08087bb44b0aa4
Roger Graves <rogergraves@gmail.com>
2020-12-04 13:20:05 -0800

    Add public #valid? method to validate whether filtering results could happen

--- app/presenters/employers_presenter/freelancers_filter.rb
+++ app/presenters/employers_presenter/freelancers_filter.rb
@@ -1,11 +1,25 @@
 module EmployersPresenter
   class FreelancersFilter
-    def initialize(sector_ids: [], skill_ids: [], software_ids: [])
+    def initialize(sector_ids: [], real_estate_skill_ids: [], software_ids: [])
+      @sector_ids = sector_ids
+      @real_estate_skill_ids = real_estate_skill_ids
+      @software_ids = software_ids
 
+      scrub_ids!
     end
 
     def valid?
+      return false if @sector_ids.empty? && @real_estate_skill_ids.empty? && @software_ids.empty?
+
       true
+    end
+
+    private
+
+    def scrub_ids!
+      @sector_ids = Sector.enabled.where(id: @sector_ids).pluck(:id) if @sector_ids.any?
+      @real_estate_skill_ids = RealEstateSkill.enabled.where(id: @real_estate_skill_ids).pluck(:id) if @real_estate_skill_ids.any?
+      @software_ids = Software.enabled.where(id: @software_ids).pluck(:id) if @software_ids.any?
     end
   end
 end
--- spec/presenters/employers_presenter/freelancer_filter_spec.rb
+++ spec/presenters/employers_presenter/freelancer_filter_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 describe EmployersPresenter::FreelancersFilter do
   let!(:freelancer_with_obscure_sector) { FactoryBot.create(:freelancer_profile, :complete) }
-  let!(:freelancer_with_obscure_skill) { FactoryBot.create(:freelancer_profile, :complete) }
+  let!(:freelancer_with_obscure_real_state_skill) { FactoryBot.create(:freelancer_profile, :complete) }
   let!(:freelancer_with_obscure_software) { FactoryBot.create(:freelancer_profile, :complete) }
 
-  let!(:obscure_sector) { FactoryBot.create(:sector) }
-  let!(:obscure_skill) { FactoryBot.create(:skill) }
-  let!(:obscure_software) { FactoryBot.create(:software) }
+  let!(:obscure_sector) { FactoryBot.create(:sector, description: 'Strange Sector') }
+  let!(:obscure_real_state_skill) { FactoryBot.create(:real_estate_skill, description: 'Strange Skill') }
+  let!(:obscure_software) { FactoryBot.create(:software, description: 'Strange Software Program') }
 
   it 'instantiates' do
     expect(EmployersPresenter::FreelancersFilter.new).to be_truthy
@@ -19,7 +19,7 @@ describe EmployersPresenter::FreelancersFilter do
         expect(EmployersPresenter::FreelancersFilter.new(sector_ids:[obscure_sector.id]).valid?).to be_truthy
       end
       it 'a valid skill_id is passed' do
-        expect(EmployersPresenter::FreelancersFilter.new(skill_ids:[obscure_skill.id]).valid?).to be_truthy
+        expect(EmployersPresenter::FreelancersFilter.new(real_estate_skill_ids:[obscure_real_state_skill.id]).valid?).to be_truthy
       end
       it 'a valid software_id is passed' do
         expect(EmployersPresenter::FreelancersFilter.new(software_ids:[obscure_software.id]).valid?).to be_truthy
@@ -27,11 +27,17 @@ describe EmployersPresenter::FreelancersFilter do
     end
 
     context 'returns false if' do
+      it 'nothing is passed' do
+        expect(EmployersPresenter::FreelancersFilter.new().valid?).to be_falsey
+      end
 
+      it 'empty arrays are passed' do
+        expect(EmployersPresenter::FreelancersFilter.new(sector_ids: [], real_estate_skill_ids: [], software_ids: []).valid?).to be_falsey
+      end
+
+      it 'no valid ids are passed' do
+        expect(EmployersPresenter::FreelancersFilter.new(sector_ids: [999], real_estate_skill_ids: [888], software_ids: [777]).valid?).to be_falsey
+      end
     end
-    it 'returns false if nothing is passed to it, or no recognized sectors, skills and softwares' do
-
-    end
-
   end
 end
