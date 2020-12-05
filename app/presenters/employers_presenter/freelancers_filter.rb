5075efe949ed6a290f61023710fcd8edf89ad9e0
Roger Graves <rogergraves@gmail.com>
2020-12-04 18:23:48 -0800

    Finish out filters

--- app/presenters/employers_presenter/freelancers_filter.rb
+++ app/presenters/employers_presenter/freelancers_filter.rb
@@ -9,9 +9,17 @@ module EmployersPresenter
     end
 
     def valid?
-      return false if @sector_ids.empty? && @real_estate_skill_ids.empty? && @software_ids.empty?
+      @sector_ids.any? || @real_estate_skill_ids.any? || @software_ids.any?
+    end
 
-      true
+    def freelancer_profile_ids
+      return [] unless valid?
+      matches = {}
+      (sector_fp_ids + skills_fp_ids + software_fp_ids).each do |fp_id|
+        matches[fp_id] = matches[fp_id].to_i.succ
+      end
+
+      matches.reject {|_fp_id, count| count < total_matches_needed}.keys
     end
 
     private
@@ -20,6 +28,22 @@ module EmployersPresenter
       @sector_ids = Sector.enabled.where(id: @sector_ids).pluck(:id) if @sector_ids.any?
       @real_estate_skill_ids = RealEstateSkill.enabled.where(id: @real_estate_skill_ids).pluck(:id) if @real_estate_skill_ids.any?
       @software_ids = Software.enabled.where(id: @software_ids).pluck(:id) if @software_ids.any?
+    end
+
+    def sector_fp_ids
+      @sector_fp_ids ||= FreelancerSector.where(sector_id: @sector_ids).pluck(:freelancer_profile_id)
+    end
+
+    def skills_fp_ids
+      @skills_fp_ids ||= FreelancerRealEstateSkill.where(real_estate_skill_id: @real_estate_skill_ids).pluck(:freelancer_profile_id)
+    end
+
+    def software_fp_ids
+      @software_fp_ids ||= FreelancerSoftware.where(software_id: @software_ids).pluck(:freelancer_profile_id)
+    end
+
+    def total_matches_needed
+      @total_matches_needed ||= (@sector_ids.count + @real_estate_skill_ids.count + @software_ids.count)
     end
   end
 end
--- spec/presenters/employers_presenter/freelancer_filter_spec.rb
+++ spec/presenters/employers_presenter/freelancer_filter_spec.rb
@@ -9,6 +9,17 @@ describe EmployersPresenter::FreelancersFilter do
   let!(:obscure_real_state_skill) { FactoryBot.create(:real_estate_skill, description: 'Strange Skill') }
   let!(:obscure_software) { FactoryBot.create(:software, description: 'Strange Software Program') }
 
+  let!(:obscure_freelancer_sector) { FactoryBot.create(:freelancer_sector, freelancer_profile: freelancer_with_obscure_sector, sector: obscure_sector) }
+  let!(:obscure_freelancer_real_state_skill) { FactoryBot.create(:freelancer_real_estate_skill, freelancer_profile: freelancer_with_obscure_real_state_skill, real_estate_skill: obscure_real_state_skill) }
+  let!(:obscure_freelancer_software) { FactoryBot.create(:freelancer_software, freelancer_profile: freelancer_with_obscure_software, software: obscure_software) }
+
+
+  it 'factories are set up properly' do
+    expect(freelancer_with_obscure_sector.sectors).to include(obscure_sector)
+    expect(freelancer_with_obscure_real_state_skill.real_estate_skills).to include(obscure_real_state_skill)
+    expect(freelancer_with_obscure_software.softwares).to include(obscure_software)
+  end
+
   it 'instantiates' do
     expect(EmployersPresenter::FreelancersFilter.new).to be_truthy
   end
@@ -37,6 +48,60 @@ describe EmployersPresenter::FreelancersFilter do
 
       it 'no valid ids are passed' do
         expect(EmployersPresenter::FreelancersFilter.new(sector_ids: [999], real_estate_skill_ids: [888], software_ids: [777]).valid?).to be_falsey
+      end
+    end
+  end
+
+  describe '#freelancer_profile_ids' do
+    let(:freelancer_who_passes) { FactoryBot.create(:freelancer_profile) }
+
+    context 'only one object of one type is passed' do
+      let!(:filter) { EmployersPresenter::FreelancersFilter.new(sector_ids: [obscure_sector.id]) }
+      it 'returns freelancers who match it' do
+        expect(filter.freelancer_profile_ids).to include(freelancer_with_obscure_sector.id)
+      end
+      it 'does not return freelancers who do not match it' do
+        expect(filter.freelancer_profile_ids).to_not include(freelancer_with_obscure_real_state_skill.id)
+        expect(filter.freelancer_profile_ids).to_not include(freelancer_with_obscure_software.id)
+      end
+    end
+
+    context 'one tag each for all the filters' do
+      let!(:freelancer_who_passes_obscure_sector) { FactoryBot.create(:freelancer_sector, freelancer_profile: freelancer_who_passes, sector: obscure_sector) }
+      let!(:freelancer_who_passes_obscure_real_estate_skill) { FactoryBot.create(:freelancer_real_estate_skill, freelancer_profile: freelancer_who_passes, real_estate_skill: obscure_real_state_skill) }
+      let!(:freelancer_who_passes_obscure_software) { FactoryBot.create(:freelancer_software, freelancer_profile: freelancer_who_passes, software: obscure_software) }
+
+      let(:filter) { EmployersPresenter::FreelancersFilter.new(sector_ids: [obscure_sector.id], real_estate_skill_ids: [obscure_real_state_skill.id], software_ids: [obscure_software.id]) }
+
+      it 'returns only freelancers who match all three filter entries' do
+        expect(filter.freelancer_profile_ids).to match_array([freelancer_who_passes.id])
+      end
+    end
+
+    context 'two tags of one type of filter' do
+      let!(:obscure_sector2) { FactoryBot.create(:sector, description: 'Strange Sector 2') }
+      let!(:freelancer_who_passes_obscure_sector) { FactoryBot.create(:freelancer_sector, freelancer_profile: freelancer_who_passes, sector: obscure_sector) }
+      let!(:freelancer_who_passes_obscure_sector2) { FactoryBot.create(:freelancer_sector, freelancer_profile: freelancer_who_passes, sector: obscure_sector2) }
+
+      let(:filter) { EmployersPresenter::FreelancersFilter.new(sector_ids: [obscure_sector.id, obscure_sector2.id]) }
+
+      it 'returns only freelancers who match all both of the same time of passed filters' do
+        expect(filter.freelancer_profile_ids).to match_array([freelancer_who_passes.id])
+      end
+    end
+
+    context 'two tags of two types of filters' do
+      let!(:obscure_sector2) { FactoryBot.create(:sector, description: 'Strange Sector 2') }
+      let!(:obscure_software2) { FactoryBot.create(:software, description: 'Strange Software Program 2') }
+      let!(:freelancer_who_passes_obscure_sector) { FactoryBot.create(:freelancer_sector, freelancer_profile: freelancer_who_passes, sector: obscure_sector) }
+      let!(:freelancer_who_passes_obscure_sector2) { FactoryBot.create(:freelancer_sector, freelancer_profile: freelancer_who_passes, sector: obscure_sector2) }
+      let!(:freelancer_who_passes_obscure_software) { FactoryBot.create(:freelancer_software, freelancer_profile: freelancer_who_passes, software: obscure_software) }
+      let!(:freelancer_who_passes_obscure_software2) { FactoryBot.create(:freelancer_software, freelancer_profile: freelancer_who_passes, software: obscure_software2) }
+
+      let(:filter) { EmployersPresenter::FreelancersFilter.new(sector_ids: [obscure_sector.id, obscure_sector2.id], software_ids: [obscure_software.id, obscure_software2.id]) }
+
+      it 'returns freelancer who match all' do
+        expect(filter.freelancer_profile_ids).to match_array([freelancer_who_passes.id])
       end
     end
   end
