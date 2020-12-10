e1a7a81fbf84973dd326d8d7fa06fc5d4e1cf2ac
“SBorozenets” <s.borozenets@gmail.com>
2020-12-10 18:42:27 +0200

    changes after review

--- app/controllers/freelancer/application_flows_controller.rb
+++ app/controllers/freelancer/application_flows_controller.rb
@@ -25,9 +25,6 @@ class Freelancer::ApplicationFlowsController < ApplicationController
   def application_step_1_save
     return false unless params[:job_application][:step] == 'application_step_1'
 
-    if params[:job_application][:template].to_i == 1 && @user.job_applications.find_by(template: true).present?
-      @user.job_applications.find_by(template: true).update(template: false)
-    end
     job_application.update(step_1_params)
 
     respond_js_format(:application_step_2)
@@ -38,18 +35,15 @@ class Freelancer::ApplicationFlowsController < ApplicationController
   def application_step_2_save
     return false unless params[:job_application][:step] == 'application_step_2'
 
-    per_hour_bid = params[:job_application][:per_hour_bid].split('$').reject(&:empty?).join.to_i
-    available_during_work_hours = params[:job_application][:available_during_work_hours] == 'yes' ? true : false
-    job_application.job.job_questions.each do |q|
-      JobApplicationQuestion.create(
-        job_question_id: q.id,
+    job_application.job.job_questions.each do |job_question|
+      job_question.job_application_questions.create(
         job_application_id: job_application.id,
-        answer: params["job_question_#{q.id}"]
+        answer: params["job_question_#{job_question.id}"]
       )
     end
     job_application.update(
-      per_hour_bid: per_hour_bid,
-      available_during_work_hours: available_during_work_hours
+      per_hour_bid: cleaned_per_hour_bid,
+      available_during_work_hours: cleaned_available_during_work_hours
     )
     job_application.update(state: 'draft') if params[:button] == 'draft'
 
@@ -76,5 +70,13 @@ class Freelancer::ApplicationFlowsController < ApplicationController
   def job_application
     @job_application ||= current_user.job_applications
                            .find_by(id: (params[:job_app] || params[:job_application][:job_app_id]))
+  end
+
+  def cleaned_per_hour_bid
+    params[:job_application][:per_hour_bid].split('$').reject(&:empty?).join.to_i
+  end
+
+  def cleaned_available_during_work_hours
+    params[:job_application][:available_during_work_hours] == 'yes' ? true : false
   end
 end
--- app/controllers/work_sample_controller.rb
+++ app/controllers/work_sample_controller.rb
@@ -5,26 +5,32 @@ class WorkSampleController < ApplicationController
   before_action :authenticate_user!
 
   def update
-    @user = current_user
-    @job_application = @user.job_applications.find_by(id: params[:app_id])
-    @job_application.work_sample.purge_later if params[:work_sample].present? && @job_application.work_sample.attached?
-    @job_application.work_sample.attach(params[:work_sample]) if params[:work_sample].present?
+    job_application.work_sample.purge_later if params[:work_sample].present? && job_application.work_sample.attached?
+    job_application.work_sample.attach(params[:work_sample]) if params[:work_sample].present?
 
     render json: { status: :ok,
                    file_name: @job_application.work_sample.filename.to_s
                  }
   rescue StandardError
-    @job_app.work_sample.purge_later
-    @errors.add(:base, 'File did not attach')
+    job_application.work_sample.purge_later
+    @errors.add(:base, 'File was not uploaded successfully.')
   end
 
   def destroy
-    @user = current_user
-    @job_application = @user.job_applications.find_by(id: params[:app_id])
-    return unless @job_application.work_sample.attached?
+    return unless job_application.work_sample.attached?
 
-    @job_application.work_sample.purge_later
+    job_application.work_sample.purge_later
 
     render json: { status: :ok }
+  end
+
+  private
+
+  def user
+    @user ||= current_user
+  end
+
+  def job_application
+    @job_application ||= user.job_applications.find_by(id: params[:app_id])
   end
 end
