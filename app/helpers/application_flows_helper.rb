09c82740509dc1033a0ec1a44a0d7dd55302b6ab
Roger Graves <rogergraves@gmail.com>
2020-12-11 12:17:03 -0800

    Use RegEx for filtering out stuff

--- app/helpers/application_flows_helper.rb
+++ app/helpers/application_flows_helper.rb
@@ -2,16 +2,6 @@
 
 module ApplicationFlowsHelper
   def cleaned_per_hour_bid(per_hour_bid)
-    first_el = per_hour_bid.chars.first.to_i
-    end_el = per_hour_bid.chars.last.to_i
-
-    if first_el.zero?
-      cleaned_bid = per_hour_bid[1..(per_hour_bid.size - 1)]
-    elsif end_el.zero? && per_hour_bid.chars.last != '0'
-      cleaned_bid = per_hour_bid[0..(per_hour_bid.size - 2)]
-    else
-      cleaned_bid = per_hour_bid
-    end
-    cleaned_bid.delete(',').to_i
+    per_hour_bid.gsub(/\$|a..zA..Z|,/, '').to_i
   end
 end
