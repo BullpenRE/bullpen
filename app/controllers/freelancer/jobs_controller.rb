bf4f0ef39eb498b50c83a61d40ae9725564ee738
Roger Graves <rogergraves@gmail.com>
2020-12-14 15:46:18 -0800

    Create partials for pagy navigation

--- app/controllers/freelancer/jobs_controller.rb
+++ app/controllers/freelancer/jobs_controller.rb
@@ -2,11 +2,13 @@
 
 class Freelancer::JobsController < ApplicationController
   include LoggedInRedirects
+  include ApplicationHelper
+
   before_action :authenticate_user!, :initial_check, :non_freelancer_redirect, :incomplete_freelancer_profile_redirect
   ITEMS_PER_PAGE = 10
 
   def index
-    @pagy, @jobs = pagy(Job.where(state: 'posted'), items: ITEMS_PER_PAGE, overflow: :last_page)
+    @pagy, @jobs = pagy(Job.where(state: 'posted'), items: ITEMS_PER_PAGE, steps: pagy_nav_sizes, overflow: :last_page)
     @job_applications = current_user.job_applications
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,12 @@ module ApplicationHelper
   def select_months_array
     FreelancerProfileExperience::AVAILABLE_MONTHNAMES.each_with_index.collect { |m, i| [m, i+1] }
   end
+
+  def pagy_nav_sizes
+    {
+      0 => [0,1,1,0],
+      540 => [3,5,5,3],
+      720 => [5,7,7,5]
+    }
+  end
 end
