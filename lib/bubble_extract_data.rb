c4c79232c577aed8e65ad5eb75054bced751a658
Agandaur <cjwagstaffe@gmail.com>
2020-12-15 03:35:15 -0800

    Both #retrieve and #retrieve_all call the API successfully
    The #retrieve method takes a cursor parameter, but coresponds to the index of the data in the returned list and not the page number (as I thought previously). For example, calling BubbleExtractData.new('user').retrieve(0) would return data from index 0-99, where calling BubbleExtractData.new('user').retrieve(1) would return data from index 1-100. This increase is reflected in the #retrieve_all and subsequient #repeat_request methods.

--- lib/bubble_extract_data.rb
+++ lib/bubble_extract_data.rb
@@ -1,38 +1,43 @@
 class Bubble_extract_data
+    @data = []
+
     def initialize(type)
         @type = type
     end
 
-    # def request_users
-    #     users = request(1)
-    #     body = JSON.parse(users.body)
-        
-    #     if users.success?
-    #         upload_users(body)
-    #     else 
-    #         "#{body["statusCode"]}: #{body["body"]["status"]} -- #{body["body"]["message"]}"
-    #     end
-    # end
-
     def retrieve(cursor=0)
+        call = request(cursor)
+        body = JSON.parse(call.body)
+        raw_data = body['response']['results']
 
+        if call.success? && raw_data.length
+            @data = raw_data
+            return true
+        else
+            return false
+        end
     end
 
     def retrieve_all
-
+        @data = []
+        return repeat_request(0)
     end
 
     def process
-
+        
     end
 
     def find_by(field, value)
-        
+
+    end
+
+    def view_data 
+        @data
     end
 
     private
     def client
-        connection ||= Faraday.new(
+        connection = Faraday.new(
             url: ENV['BUBBLE_API_URL']
         )
     end
@@ -40,6 +45,23 @@ class Bubble_extract_data
     def request(cursor=0)
         response = client.get(@type) do |req|
           req.params['cursor'] = cursor
+        end
+    end
+
+    # recursively call the API until all results for desired obj are retrieved
+    def repeat_request(cursor)
+        call = request(cursor)
+        body = JSON.parse(call.body)
+        raw_data = body['response']['results']
+        
+        if call.success? && raw_data.length != 0 # check if the call was successfull and there are results
+            @data = @data.concat(raw_data)
+            remaining = body['response']['remaining']
+            return repeat_request(cursor + 100) if remaining
+        elsif call.success? && raw_data.length == 0 # catch for when the call is successfull and there are no further results
+            return true
+        else
+            return false
         end
     end
 end
