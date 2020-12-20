fd46592f1c18125e5d2469a37c36338f51f6daac
leksus1966 <alexsokalo@gmail.com>
2020-12-20 12:15:25 +0200

    revert back, remove jquery-slimscroll

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,6 @@
 // that code so it'll be compiled.
 
 require("jquery")
-require("jquery-slimscroll")
 require("@fortawesome/fontawesome-free/js/all")
 require("@rails/ujs").start()
 require("turbolinks").start()
