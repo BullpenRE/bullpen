c1a8c1687b453868a60540edac2a8de38128c7b5
GalinaKr <galinakr@github.com>
2020-12-22 21:02:19 +0200

    fix codeclimate

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,9 +11,10 @@ module ApplicationHelper
 
   def mobile_device
     agent = request.user_agent
-    
+
     return 'tablet' if agent =~ /(tablet|ipad)|(android(?!.*mobile))/i
     return 'mobile' if agent =~ /Mobile/
-    return 'desktop'
+
+    'desktop'
   end
 end
