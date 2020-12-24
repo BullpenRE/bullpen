052ad7567d867d84ad9ce229d5d751edb3448f37
“SBorozenets” <s.borozenets@gmail.com>
2020-12-24 15:33:04 +0200

    Allow inline editing of skills

--- app/controllers/freelancer/profile_controller.rb
+++ app/controllers/freelancer/profile_controller.rb
@@ -2,6 +2,21 @@
 
 class Freelancer::ProfileController < ApplicationController
   include LoggedInRedirects
-  before_action :authenticate_user!, :initial_check, :non_freelancer_redirect, :incomplete_freelancer_profile_redirect
+  before_action :authenticate_user!, :initial_check, :non_freelancer_redirect
 
+  def change_skills
+    @freelancer_profile = current_user.freelancer_profile
+    @freelancer_profile&.freelancer_real_estate_skills&.destroy_all
+    real_estate_skill_params&.each do |skill|
+      @freelancer_profile.freelancer_real_estate_skills.create(real_estate_skill_id: skill)
+    end
+
+    redirect_to freelancer_profile_step_path(:summary)
+  end
+
+  private
+
+  def real_estate_skill_params
+    params[:freelancer_profile][:freelancer_real_estate_skills]&.reject(&:blank?)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -58,6 +58,7 @@ Rails.application.routes.draw do
     resources :interviews
     resources :contracts
     resource :profile
+    post 'change_skills', to: 'profile#change_skills'
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
