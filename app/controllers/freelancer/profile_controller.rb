f541ac997811488bdbdcc51d2e361eb8f18624d3
GalinaKr <galinakr@github.com>
2020-12-24 21:03:13 +0200

    BP-175 Allow inline editing of software licenses

--- app/controllers/freelancer/profile_controller.rb
+++ app/controllers/freelancer/profile_controller.rb
@@ -2,6 +2,22 @@
 
 class Freelancer::ProfileController < ApplicationController
   include LoggedInRedirects
-  before_action :authenticate_user!, :initial_check, :non_freelancer_redirect, :incomplete_freelancer_profile_redirect
+  before_action :authenticate_user!, :initial_check, :non_freelancer_redirect
+
+  def change_software_licence
+    @freelancer_profile = current_user.freelancer_profile
+    @freelancer_profile&.freelancer_softwares&.destroy_all
+    software_params&.each do |software|
+      @freelancer_profile.freelancer_softwares.create(software_id: software)
+    end
+
+    redirect_to freelancer_profile_step_path(:summary)
+  end
+
+  private
+
+  def software_params
+    params[:freelancer_softwares][:freelancer_softwares]
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -58,6 +58,7 @@ Rails.application.routes.draw do
     resources :interviews
     resources :contracts
     resource :profile
+    post 'change_software_licence', to: 'profile#change_software_licence'
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
