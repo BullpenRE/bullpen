5b586c62e73bd55f0e0e6d52f1f6d241ff40e11e
GalinaKr <galinakr@github.com>
2020-12-25 09:23:57 +0200

    add form for edit, rout and some code

--- app/controllers/freelancer/profile_controller.rb
+++ app/controllers/freelancer/profile_controller.rb
@@ -2,6 +2,27 @@
 
 class Freelancer::ProfileController < ApplicationController
   include LoggedInRedirects
+  include WorkEducationExperience
   before_action :authenticate_user!, :initial_check, :non_freelancer_redirect, :incomplete_freelancer_profile_redirect
 
+  def change_certifications
+    @freelancer_profile = current_user.freelancer_profile
+    @freelancer_profile&.freelancer_certifications&.destroy_all
+    certifications_params&.each do |certifications|
+      @freelancer_profile.freelancer_certifications.create(certifications_id: certifications)
+    end
+
+    redirect_to freelancer_profile_step_path(:summary)
+  end
+
+  private
+
+  def certifications_params
+    byebug
+    params[:freelancer_profile][:freelancer_certifications]&.reject(&:blank?)
+  end
+
+  def freelancer_certification
+    @freelancer_profile&.freelancer_certifications&.find_by(id: params[:freelancer_certification][:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -58,6 +58,7 @@ Rails.application.routes.draw do
     resources :interviews
     resources :contracts
     resource :profile
+    post 'change_certifications', to: 'profile#change_certifications'
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
