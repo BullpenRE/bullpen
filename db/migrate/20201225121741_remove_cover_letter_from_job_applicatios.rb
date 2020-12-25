7e83de8fb785220937d7bae75a3f1a239eb32a1e
Roger Graves <rogergraves@gmail.com>
2020-12-25 09:07:10 -0800

    Add type so a rollback is possible

--- db/migrate/20201225121741_remove_cover_letter_from_job_applicatios.rb
+++ db/migrate/20201225121741_remove_cover_letter_from_job_applicatios.rb
@@ -1,5 +1,5 @@
 class RemoveCoverLetterFromJobApplicatios < ActiveRecord::Migration[6.0]
   def change
-    remove_column :job_applications, :cover_letter
+    remove_column :job_applications, :cover_letter, :text
   end
 end
