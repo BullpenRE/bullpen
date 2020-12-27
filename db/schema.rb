804c380371189e81f0bc867e9957e26e58e04871
SBorozenets <s.borozenets@gmail.com>
2020-12-27 19:36:05 +0200

    create heart icons and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_25_121741) do
+ActiveRecord::Schema.define(version: 2020_12_27_172000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -218,6 +218,7 @@ ActiveRecord::Schema.define(version: 2020_12_25_121741) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.datetime "applied_at", precision: 6
+    t.boolean "liked", default: false
     t.index ["job_id"], name: "index_job_applications_on_job_id"
     t.index ["user_id"], name: "index_job_applications_on_user_id"
   end
