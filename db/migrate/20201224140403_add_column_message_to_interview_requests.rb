8a637d01c1ec9a36a4feb23a0728ccb63afdf73e
Goncharenko2013 <tgoncharenko@gmail.com>
2020-12-28 17:39:41 +0200

    BP-169 Two already needless migrations were deleted

--- db/migrate/20201224140403_add_column_message_to_interview_requests.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnMessageToInterviewRequests < ActiveRecord::Migration[6.0]
-  def change
-    add_column :interview_requests, :message, :text
-  end
-end
--- db/migrate/20201228140205_remove_column_message_from_interview_requests.rb
+++
@@ -1,5 +0,0 @@
-class RemoveColumnMessageFromInterviewRequests < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :interview_requests, :message, :text
-  end
-end
