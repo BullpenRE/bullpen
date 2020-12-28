e750bf98659499c144207a15452f4b516a4f231b
SBorozenets <s.borozenets@gmail.com>
2020-12-28 13:54:47 +0200

    rename and fix migration

--- db/migrate/20201227172000_add_liked_to_job_applications.rb
+++ db/migrate/20201227172000_add_liked_to_job_applications.rb
@@ -1,4 +1,4 @@
-class AddLikedToJobAplication < ActiveRecord::Migration[6.0]
+class AddLikedToJobApplication < ActiveRecord::Migration[6.0]
   def change
     add_column :job_applications, :liked, :boolean, default: false
   end
