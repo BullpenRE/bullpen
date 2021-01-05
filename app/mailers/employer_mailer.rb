63d258cfbfedf2794f658a41feb7011fe12f30a6
Goncharenko2013 <tgoncharenko@gmail.com>
2021-01-05 16:23:29 +0200

    BP-174 job's view after job_application withdrawn was fixed

--- app/mailers/employer_mailer.rb
+++ app/mailers/employer_mailer.rb
@@ -11,7 +11,7 @@ class EmployerMailer < ApplicationMailer
 
   def job_application_was_withdrawn(job_application)
     @job_application = job_application
-    @url = employer_jobs_url
+    @url = employer_jobs_url(view_job: @job_application.job.id)
     mail(to: @job_application.job.user.email, subject: "#{@job_application.user.full_name} withdrew their application")
   end
 end
