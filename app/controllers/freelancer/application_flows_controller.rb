c319962b0c706df8983a895e2312e18d9c41fb43
Goncharenko2013 <tgoncharenko@gmail.com>
2021-01-06 19:41:39 +0200

    BP-174 fix to meet codeclimate suggestions

--- app/controllers/freelancer/application_flows_controller.rb
+++ app/controllers/freelancer/application_flows_controller.rb
@@ -71,7 +71,8 @@ class Freelancer::ApplicationFlowsController < ApplicationController
       job_application.update(state: 'draft')
       draft_flash_notice!
     else
-      EmployerMailer.new_job_application(current_user, job_application).deliver_now unless job_application.applied_at.present?
+      EmployerMailer.new_job_application(current_user, job_application)
+                    .deliver_now unless job_application.applied_at.present?
       job_application.update(state: 'applied', applied_at: Time.current)
       apply_flash_notice!
 
