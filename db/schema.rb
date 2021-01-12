ab2a03d938ab722e62be6963eb7f42f1291c557e
Roger Graves <rogergraves@gmail.com>
2021-01-11 18:13:34 -0800

    Add profile and uid fields to User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_01_085957) do
+ActiveRecord::Schema.define(version: 2021_01_12_021221) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -341,6 +341,8 @@ ActiveRecord::Schema.define(version: 2021_01_01_085957) do
     t.string "location"
     t.integer "role"
     t.bigint "signup_promo_id"
+    t.string "uid"
+    t.string "provider"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
