6b58459970a35e9d64e92202c811fcbab8d2b4d0
Roger Graves <rogergraves@gmail.com>
2021-01-12 18:57:02 -0800

    Add base and google authorizers, omniauth callbacks controller

---
+++ app/controllers/users/authorizers/google_authorizer.rb
@@ -0,0 +1,48 @@
+module Users
+  module Authorizers
+    class GoogleAuthorizer < BaseAuthorizer
+
+      def initialize(token, params)
+        @token = token
+        @params = params
+      end
+
+      def src
+        @params[:src]
+      end
+
+      def email
+        @token.email_address
+      end
+
+      def provider
+        'google'
+      end
+
+      def user_data
+        {
+          uid: uid,
+          first_name: first_name,
+          last_name: last_name,
+          email: email,
+          provider: provider
+        }
+      end
+
+      protected
+
+      def uid
+        @token.user_id
+      end
+
+      def first_name
+        @token.given_name
+      end
+
+      def last_name
+        @token.family_name
+      end
+
+    end
+  end
+end
