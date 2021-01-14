20223df5bcca11ae7ddbddf27b3d70f3096bf6f9
leksus1966 <alexsokalo@gmail.com>
2021-01-14 23:10:51 +0200

    fix bug - update logic in application_flows_controller

--- app/controllers/freelancer/application_flows_controller.rb
+++ app/controllers/freelancer/application_flows_controller.rb
@@ -107,7 +107,9 @@ class Freelancer::ApplicationFlowsController < ApplicationController
     return if application_template.blank?
 
     job_application.update(cover_letter: application_template.cover_letter) if job_application.cover_letter.blank?
-    job_application.work_sample.attach(application_template.work_sample.blob) if !job_application.work_sample.attached?
+    unless job_application.work_sample.attached? || !application_template.work_sample.attached?
+      job_application.work_sample.attach(application_template.work_sample.blob)
+    end
   end
 
   def step_2_params
