73dc735502d2436036d6bd7048693bc88cda30c1
Goncharenko2013 <tgoncharenko@gmail.com>
2021-01-14 15:52:47 +0200

    BP-199 fix to meet codeclimate suggestions

--- app/controllers/freelancer/contracts_controller.rb
+++ app/controllers/freelancer/contracts_controller.rb
@@ -4,6 +4,5 @@ class Freelancer::ContractsController < ApplicationController
   include LoggedInRedirects
   before_action :authenticate_user!, :initial_check, :non_freelancer_redirect, :incomplete_freelancer_profile_redirect
 
-  def index
-  end
+  def index; end
 end
