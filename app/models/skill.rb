4dbf9fa201e711c80055c5fb83412049301c8257
Goncharenko2013 <tgoncharenko@gmail.com>
2021-01-15 18:57:32 +0200

    BP-210 fix to meet reviewer's suggestions - all 'enabled.' have been removed from the default_scopes in the project

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Skill < ApplicationRecord
-  default_scope { enabled.order(description: :asc) }
+  default_scope { order(description: :asc) }
   scope :enabled, -> { where.not(disable: true) }
 
   has_many :job_skills, dependent: :destroy
