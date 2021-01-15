cfaca037c0a905821872f3b739f6cf354fec3836
Roger Graves <rogergraves@gmail.com>
2021-01-14 20:03:11 -0800

    Fix CodeClimate issue with frozen_string_literal: true

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Message < ApplicationRecord
   belongs_to :from_user, class_name: 'User', foreign_key: :from_user_id
   belongs_to :to_user, class_name: 'User', foreign_key: :to_user_id
