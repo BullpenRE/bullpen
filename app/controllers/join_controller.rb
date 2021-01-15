f261dedc49e1af1143d7f444e6ceac4e780c0289
Roger Graves <rogergraves@gmail.com>
2021-01-15 13:55:11 -0800

    Replace the join page with email entry page, refactor

--- app/controllers/join_controller.rb
+++ app/controllers/join_controller.rb
@@ -6,6 +6,20 @@ class JoinController < ApplicationController
 
   def index; end
 
+  def signup
+    if User.find_by(email: user_email)
+      flash[:alert] = I18n.t 'devise.registrations.already_exists', email: user_email, path: new_user_session_path
+    end
+
+    redirect_to join_path
+  end
+
+  private
+
+  def user_email
+    params[:user][:email]
+  end
+
   def check_signed_in
     redirect_to current_signup_step_url if user_signed_in?
   end
