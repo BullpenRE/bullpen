10b3d69ab8a0c43bae68adb24c206e6073330ca2
leksus1966 <alexsokalo@gmail.com>
2021-01-21 22:52:06 +0200

    [BP-220] task3 done

--- app/controllers/freelancer/profile/preferences_controller.rb
+++ app/controllers/freelancer/profile/preferences_controller.rb
@@ -3,23 +3,23 @@
 module Freelancer
   module Profile
     class PreferencesController < ApplicationController
-      before_action :set_freelancer_profile
+      before_action :authenticate_user!, :set_freelancer_profile
 
       def update
         is_updated = if params[:field] == 'new_jobs_alert'
-                       @freelancer_profile.update(new_jobs_alert: params[:value])
+                       @freelancer_profile.update!(new_jobs_alert: params[:value])
                      elsif params[:field] == 'searchable'
-                       @freelancer_profile.update(searchable: params[:value])
+                       @freelancer_profile.update!(searchable: params[:value])
                      else
                        false
                      end
 
-        if is_updated
-          respond_to do |format|
+        respond_to do |format|
+          if is_updated
             format.json { render json: @freelancer_profile, status: :ok }
+          else
+            format.json {render json: @freelancer_profile.errors, status: :unprocessable_entity }
           end
-        else
-          render json: {}, status: :bad_request
         end
       end
 
