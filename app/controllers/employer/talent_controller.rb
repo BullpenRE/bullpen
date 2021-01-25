4cd1e030fcf4de723cadd4cf4a01d39836e10f07
Roger Graves <rogergraves@gmail.com>
2021-01-25 15:37:09 -0800

    Add flash message after message is modified

--- app/controllers/employer/talent_controller.rb
+++ app/controllers/employer/talent_controller.rb
@@ -25,6 +25,9 @@ class Employer::TalentController < ApplicationController
     if interview_id.present?
       @interview_request = current_user.employer_profile.interview_requests.find_by(id: interview_id)
       @interview_request.update(message: params[:interview_request][:message])
+      flash[:notice] = 'Your interview request message was successfully modified for '\
+                       "<b>#{@interview_request.freelancer_profile.full_name}</b>. "\
+                       'No new emails were sent but they will see the new text when they review it.'
       @interview_request.update(state: 'pending') if @interview_request.withdrawn? || @interview_request.declined?
 
       redirect_to employer_interviews_path
