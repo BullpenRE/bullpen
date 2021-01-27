7e0fabdd12dfac875fe86c7bf631b781bef976df
Roger Graves <rogergraves@gmail.com>
2021-01-26 22:27:00 -0800

    When users log out, redirect them to sign in page instead of sign up page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,10 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
+
   private
 
   def current_freelancer_profile_step(user)
