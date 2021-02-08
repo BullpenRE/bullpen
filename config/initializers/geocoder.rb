54132e8092e67df1e7bae57c634bcd103fc99d88
leksus1966 <alexsokalo@gmail.com>
2021-02-08 21:25:47 +0200

    [BP-244] add stub for geocoder api calls

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -20,3 +20,29 @@ Geocoder.configure(
   # units: :mi,                 # :km for kilometers or :mi for miles
   # distances: :linear          # :spherical or :linear
 )
+
+Geocoder::Lookup::Test.add_stub(
+  'New York, NY', [
+    {
+      'coordinates' => [40.7143528, -74.0059731],
+      'address' => 'New York, NY, USA',
+      'state' => 'New York',
+      'state_code' => 'NY',
+      'country' => 'United States',
+      'country_code' => 'US'
+    }
+  ]
+)
+
+Geocoder::Lookup::Test.set_default_stub(
+  [
+    {
+      'coordinates' => [40.7143528, -74.0059731],
+      'address' => 'New York, NY, USA',
+      'state' => 'New York',
+      'state_code' => 'NY',
+      'country' => 'United States',
+      'country_code' => 'US'
+    }
+  ]
+)
