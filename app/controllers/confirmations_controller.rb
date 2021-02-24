9055a75f7401bea48d5c6ce0fcb607c48154b046
GalinaKr <kravchukgi@gmail.com>
2021-02-24 11:52:28 +0200

    ad tracking to sign in and sign up pages, Validating account

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -3,7 +3,12 @@
 class ConfirmationsController < Devise::ConfirmationsController
   def show
     super do |resource|
+      mixpanel_confirmation_tracker
       sign_in(resource)
     end
+  end
+
+  def mixpanel_confirmation_tracker
+    MixpanelWorker.perform_async(current_user.id, 'Validating account', { 'user': current_user.email })
   end
 end
