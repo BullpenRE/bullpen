ae7e697fb7d8202bec406049e8eace21f7b31cf1
Roger Graves <rogergraves@gmail.com>
2021-02-23 17:57:36 -0700

    Refactor connect method

--- app/controllers/stripe_controller.rb
+++ app/controllers/stripe_controller.rb
@@ -2,25 +2,13 @@
 
 class StripeController < ApplicationController
   before_action :authenticate_user!, :initial_check, :non_freelancer_redirect, :freelancer_profile
+  before_action :stripe_response_error, only: [:connect]
 
-  def connect # rubocop:disable Metrics/MethodLength
-    response = HTTParty.post('https://connect.stripe.com/oauth/token',
-                             query: {
-                               client_secret: ENV['STRIPE_SECRET_KEY'],
-                               code: params[:code],
-                               grant_type: 'authorization_code'
-                             })
+  def connect
+    stripe_user_id = stripe_response.parsed_response['stripe_user_id']
+    freelancer_profile.update_attribute(:stripe_id_account, stripe_user_id)
 
-    if response.parsed_response.key?('error')
-      redirect_to freelancer_account_index_path,
-                  notice: response.parsed_response['error_description']
-    else
-      stripe_user_id = response.parsed_response['stripe_user_id']
-      freelancer_profile.update_attribute(:stripe_id_account, stripe_user_id)
-
-      redirect_to freelancer_account_index_path,
-                  notice: 'Connected with Stripe successfully'
-    end
+    redirect_to freelancer_account_index_path, notice: 'Connected with Stripe successfully'
   end
 
   def dashboard
@@ -37,6 +25,21 @@ class StripeController < ApplicationController
   end
 
   private
+
+  def stripe_response
+    @stripe_response ||= HTTParty.post('https://connect.stripe.com/oauth/token',
+                                       query: {
+                                         client_secret: ENV['STRIPE_SECRET_KEY'],
+                                         code: params[:code],
+                                         grant_type: 'authorization_code'
+                                       })
+  end
+
+  def stripe_response_error
+    if stripe_response.parsed_response.key?('error')
+      redirect_to freelancer_account_index_path, notice: stripe_response.parsed_response['error_description']
+    end
+  end
 
   def freelancer_profile
     @freelancer_profile ||= current_user.freelancer_profile
