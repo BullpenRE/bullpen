2541e85490a1dd3f0c862275c573cbaf5e79b87c
GalinaKr <galinakr@github.com>
2021-03-08 20:43:17 +0200

    requested changes

--- app/workers/mixpanel_worker.rb
+++ app/workers/mixpanel_worker.rb
@@ -6,6 +6,8 @@ class MixpanelWorker
   include Sidekiq::Worker
 
   def perform(user_id, event_key, properties)
+    return if event_key.blank? || properties.blank?
+    
     tracker = MixpanelTracker.instance
     tracker.track(user_id, event_key, properties)
   end
