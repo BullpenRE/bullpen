32be85ee079c833494ef5e0ea69c4f79147db173
“SBorozenets” <s.borozenets@gmail.com>
2021-03-15 13:35:29 +0200

    Filters of Find Talent page dont work at all pages

---
+++ app/javascript/packs/filter_talent.js
@@ -0,0 +1,39 @@
+$(document).on('turbolinks:load', (e) => {
+  const openNav = () => {
+    const sidenavBase = document.getElementById("sidenavBase");
+    const mySidenav = document.getElementById("mySidenav");
+    sidenavBase.style.transform = 'translateX(0)';
+    mySidenav.style.border = "1px solid #e9ecef";
+    sidenavBase.classList.add("in");
+  }
+
+  const closeNav = () => {
+    const sidenavBase = document.getElementById("sidenavBase");
+    const mySidenav = document.getElementById("mySidenav");
+    sidenavBase.style.transform = 'translateX(-100%)';
+    mySidenav.style.border = "none";
+    sidenavBase.classList.remove("in");
+  }
+
+  $('#showFilters').on('click', (e) => {
+    const isOpened = document.getElementById("sidenavBase").classList.contains("in");
+    e.preventDefault();
+    if (isOpened) {
+      closeNav()
+    } else {
+      openNav()
+    }
+  });
+
+  $('#closeFilterNav').on('click', (e) => {
+    e.preventDefault();
+    closeNav()
+  });
+
+  $('#resetFilters').on('click', (e) => {
+    e.preventDefault();
+    const $select2 = $('.select2');
+    $select2.val('');
+    $select2.trigger('change');
+  });
+});
