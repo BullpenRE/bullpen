874e6797cfca1fb75ac80980cbfa1f36a71812d2
leksus1966 <alexsokalo@gmail.com>
2021-03-24 23:29:30 +0200

    [BP-333] ready for review

--- app/controllers/employer/stripe_controller.rb
+++ app/controllers/employer/stripe_controller.rb
@@ -3,86 +3,56 @@
 module Employer
   class StripeController < ApplicationController
     before_action :authenticate_user!, :initial_check, :non_employer_redirect, :employer_profile
-    before_action :stripe_response_error
 
     def create_customer
-      Stripe::Customer.create({ email: current_user.email })
-      redirect_to employer_account_index_path, notice: 'Stripe: Customer added successfully'
+      response = Stripe::CustomerCreationService.new(current_user).call
 
-    rescue StandardError => e
-      Rails.logger
-           .info "Error in employer/stripe_controller.rb#create_customer impacting user_id #{current_user.id}: #{e}"
-
-      redirect_to employer_account_index_path, alert: STRIPE_ERROR
+      if response.is_a?(Hash)
+        redirect_to employer_account_index_path, alert: STRIPE_ERROR
+      else
+        redirect_to employer_account_index_path, notice: 'Stripe: Customer added successfully'
+      end
     end
 
     def create_card
-      cus_id_absense_redirect unless employer_profile.stripe_customer_id_account.present?
+      cus_id_absense_redirect unless employer_profile.stripe_id_customer.present?
+      response = Stripe::CustomerCardCreationService.new(employer_profile.stripe_id_customer).call
 
-      card_object
-      redirect_to employer_account_index_path, notice: 'Stripe: Card added successfully'
+      if response.is_a?(Hash)
+        redirect_to employer_account_index_path, alert: STRIPE_ERROR
+      else
+        redirect_to employer_account_index_path, notice: 'Stripe: Card added successfully'
+      end
     end
 
     def create_account
-      btok_absense_redirect unless bank_token
+      btoken = Stripe::BankTokenService.new(payment_account_attributes).call
+      redirect_to(employer_account_index_path, alert: btok_absense_alert) if btoken.is_a?(Hash)
 
-      bank_account_object
-      redirect_to employer_account_index_path, notice: 'Stripe: Account added successfully'
+      response = Stripe::CustomerBankAccountService.new(employer_profile.stripe_id_customer, btoken).call
+      if response.is_a?(Hash)
+        redirect_to employer_account_index_path, alert: STRIPE_ERROR
+      else
+        redirect_to employer_account_index_path, notice: 'Stripe: Bank account added successfully'
+      end
     end
 
     private
 
-    def card_object
-      @card_object ||= Stripe::Customer.create_source(employer_profile.stripe_customer_id_account,
-                                                      { source: 'tok_mastercard' })
-    rescue StandardError => e
-      Rails.logger
-           .info "Error in employer/stripe_controller.rb#card_object impacting user_id #{current_user.id}: #{e}"
-
-      redirect_to employer_account_index_path, alert: STRIPE_ERROR
-    end
-
-    def bank_account_object
-      cus_id_absense_redirect unless employer_profile.stripe_customer_id_account.present?
-
-      @bank_account_object ||= Stripe::Customer.create_source(employer_profile.stripe_customer_id_account,
-                                                              { source: bank_token })
-    rescue StandardError => e
-      Rails.logger
-           .info "Error in employer/stripe_controller.rb#bank_account_object impacting user_id #{current_user.id}: #{e}"
-
-      redirect_to employer_account_index_path, alert: STRIPE_ERROR
-    end
-
-    def bank_token
-      return unless payment_account.present?
-
-      response = BankTokenService.new(payment_account.country,
-                                      payment_account.currency,
-                                      payment_account.account_number).call
-      if response.is_a?(Hash)
-        redirect_to employer_account_index_path, alert: STRIPE_ERROR
-      else
-        response
-      end
-    end
-
     def employer_profile
       @employer_profile ||= current_user.employer_profile
-    end
-
-    def payment_account
-      # where to get it?
-      employer_profile.payment_accounts.first
     end
 
     def cus_id_absense_redirect
       redirect_to employer_account_index_path, alert: 'Stripe: Please add a Customer ID to send payouts'
     end
 
-    def btok_absense_redirect
-      redirect_to employer_account_index_path,
-                  alert: 'There was a problem to get the bank token from Stripe. Try again later'
+    def btok_absense_alert
+      'There was a problem to get the bank token from Stripe. Try again later'
+    end
+
+    def payment_account_attributes
+      {} # there is question what to put here for now
     end
   end
 end
