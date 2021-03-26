a1e7c8af6f35d1c8589764edeeb2339c81278725
SBorozenets <s.borozenets@gmail.com>
2021-03-26 13:58:35 +0200

    Display Billing

--- app/models/billing.rb
+++ app/models/billing.rb
@@ -4,6 +4,8 @@ class Billing < ApplicationRecord
   belongs_to :contract
   enum state: { pending: 0, disputed: 1, paid: 2 }
   scope :resolved, -> { where(state: 'pending').where.not(dispute_resolved: nil) }
+  scope :current, -> { where(state: 'pending').where(dispute_resolved: nil) }
+  scope :paused, -> { where(state: 'disputed').where(dispute_resolved: nil) }
 
   validates :hours, allow_nil: true, numericality: { greater_than_or_equal_to: 0 }
   validates :minutes, allow_nil: true, numericality: { greater_than_or_equal_to: 0, less_than: 60 }
