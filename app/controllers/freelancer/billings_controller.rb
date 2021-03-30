978f1d0bfde2be92431e7f9bbe5cf33b25094dce
“SBorozenets” <s.borozenets@gmail.com>
2021-03-30 16:24:26 +0300

    after review

--- app/controllers/freelancer/billings_controller.rb
+++ app/controllers/freelancer/billings_controller.rb
@@ -3,28 +3,47 @@
 class Freelancer::BillingsController < ApplicationController
   before_action :authenticate_user!, :initial_check, :non_freelancer_redirect, :incomplete_freelancer_profile_redirect
 
-  def add_hours
+  def update_hours
     @contract = current_user.freelancer_profile.contracts.find_by(id: params[:billing][:contract_id])
-    find_billing
-    if @billing.present? && params[:commit] == 'Remove'
-      @billing.destroy
-    elsif @billing.present?
-      @billing.update(billings_params.merge(state: 'pending', dispute_resolved: Date.current)) if @billing.disputed?
-      @billing.update(billings_params)
-    else
-      @contract.billings.create(billings_params)
-    end
+    create_billing || update_billing
+
+    redirect_to freelancer_contracts_path
+  end
+
+  def destroy
+    billing = Billing.find_by(id: params[:id])
+    billing.destroy
 
     redirect_to freelancer_contracts_path
   end
 
   private
 
+  def update_billing
+    return unless billing.present?
+
+    if billing.disputed?
+      billing.update(billings_params.merge(state: 'pending', dispute_resolved: Date.current))
+    else
+      billing.update(billings_params)
+    end
+
+    true
+  end
+
+  def create_billing
+    return false if billing.present? || params[:commit] == 'Remove'
+
+    @contract.billings.create(billings_params)
+
+    true
+  end
+
   def billings_params
     params.require(:billing).permit(:work_done, :hours, :minutes, :description)
   end
 
-  def find_billing
+  def billing
     return unless params[:billing][:billing_id].present?
 
     @billing ||= @contract.billings.find_by(id: params[:billing][:billing_id])
