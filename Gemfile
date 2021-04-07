e5c952dc9435e06654b59a9feb4cb20629347c5d
leksus1966 <alexsokalo@gmail.com>
2021-04-07 23:04:22 +0300

    [BP-333] update payment account deletion logic

--- Gemfile
+++ Gemfile
@@ -58,7 +58,6 @@ group :test do
   gem 'rails-controller-testing'                             # This gem brings back assigns to your controller tests as well as assert_template to both controller and integration tests.
   gem 'stripe-ruby-mock', '~> 3.0.1', require: 'stripe_mock' # Mock and customize stripe webhooks, test against stripe errors
   gem 'shoulda-matchers'                                     # Collection of testing matchers extracted from Shoulda http://thoughtbot.com/community
-  gem 'stripe-ruby-mock', '~> 3.0.1'
   gem 'webmock', require: false
 end
 
--- app/controllers/employer/account_controller.rb
+++ app/controllers/employer/account_controller.rb
@@ -9,16 +9,29 @@ module Employer
     end
 
     def destroy
-      payment_account.destroy
-      redirect_to employer_account_index_path
+      delete_on_stripe = Stripe::Customer.delete_source(employer_profile.stripe_id_customer,
+                                                        payment_account.id_stripe)
+      (payment_account.destroy && redirect_to_index) if delete_on_stripe.deleted?
+    rescue StandardError => e
+      ::Rails.logger.info(
+        "Error in AccountController#destroy impacting user id: #{current_user.id}, "\
+        "error details: #{e}"
+      )
+      redirect_to employer_account_index_path, flash: {
+        alert: 'Payment Source was not deleted due Stripe errors'
+      }
     end
 
     def update
       payment_account.update(account_update_params)
-      redirect_to employer_account_index_path
+      redirect_to_index
     end
 
     private
+
+    def redirect_to_index
+      redirect_to employer_account_index_path
+    end
 
     def payment_account
       @payment_account ||= employer_profile.payment_accounts.find_by(id: params[:id])
