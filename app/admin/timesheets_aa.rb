ef4c0d8822a0ab125d3978071a14b07b2367edcf
leksus1966 <alexsokalo@gmail.com>
2021-04-18 12:00:36 +0300

    [BP-406] task8: add button to ActiveAdmin view

--- app/admin/timesheets_aa.rb
+++ app/admin/timesheets_aa.rb
@@ -21,7 +21,7 @@ if defined?(ActiveAdmin) && ApplicationRecord.connection.data_source_exists?('ti
     scope :ready_for_payment, -> { Timesheet.ready_for_payment }
     scope :paid, -> { Timesheet.paid }
 
-    show title: 'Timesheet' do |timesheet|
+    show title: 'Timesheet' do
       attributes_table do
         row :contract
         row :description
@@ -30,13 +30,27 @@ if defined?(ActiveAdmin) && ApplicationRecord.connection.data_source_exists?('ti
         row :created_at
         row :updated_at
         row 'Billing Entries' do
-          (timesheet.billings.order(work_done: :desc).map{|billing| link_to("#{billing.description} done on #{billing.work_done}", admin_billing_path(billing.id))}.join('<br>') + link_to('<br>Add New'.html_safe, new_admin_billing_path(:billing=> { :contract_id => timesheet.contract_id, :timesheet_id => timesheet.id }), target: '_new')).html_safe
+          (timesheet.billings.order(work_done: :desc).map { |billing| link_to("#{billing.description} done on #{billing.work_done}", admin_billing_path(billing.id))}.join('<br>') + link_to('<br>Add New'.html_safe, new_admin_billing_path(:billing=> { :contract_id => timesheet.contract_id, :timesheet_id => timesheet.id }), target: '_new')).html_safe
         end
-
+        row ' ' do |timesheet|
+          if timesheet.stripe_id_invoice.blank? && timesheet.contract.employer_profile.payment_accounts.present? && Date.current > timesheet.end
+            button_to 'Charge Employer', charge_employer_admin_timesheet_path(timesheet.id), action: :post
+          end
+        end
       end
       active_admin_comments
     end
 
+    member_action :charge_employer, method: :post do
+      timesheet = Timesheet.find_by(id: params[:id])
+      response = Stripe::Customer::InvoiceService.new(timesheet)
+
+      if response.present?
+        redirect_to admin_timesheets_path, notice: 'Invoice created'
+      else
+        redirect_to admin_timesheets_path, alert: 'Error'
+      end
+    end
 
     form do |f|
       f.inputs 'Timesheet Entry' do
